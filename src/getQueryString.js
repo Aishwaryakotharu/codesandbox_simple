cdd2947b72908ed93221041d151f1677895dea2e
Aishwaryakotharu <44223350+Aishwaryakotharu@users.noreply.github.com>
2020-02-01 11:20:25 +0530

    commit
    to_know_how_to_commit

---
+++ src/getQueryString.js
@@ -0,0 +1,12 @@
+const url = require("url");
+
+const getQueryString = urlString => {
+  try {
+    return url.parse(urlString, true).query;
+  } catch (e) {
+    console.error(e);
+    return {};
+  }
+};
+//return parameter : key n value like a func but exported so called module
+module.exports = getQueryString;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-var http = require("http");
+const http = require("http"); //common js model,es6 supported by v13
+const getQueryString = require("./getQueryString");
 
-//create a server object:
+//process has methods(of that module) ,properties,functions
+console.log("hi");
+
 http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
+  .createServer((request, response) => {
+    //res.writeHead(200,{"Content-Type":"text/html"});
+    response.writeHead(200, { "Content-Type": "application/json" }); //MIME TYPE
+    const queryObject = getQueryString(request.url);
+    response.write(JSON.stringify(queryObject));
+    response.end();
+    //res.write("hello..");
   })
-  .listen(8080); //the server object listens on port 8080
+  //console.log("wait");
+  .listen(8080);
+//server will listen to request port
+
+//wikipedia.com/books/?isbn=20034324

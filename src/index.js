1f47b6c7ef9a47bff65604384af434c154c9a50a
Aishwaryakotharu <44223350+Aishwaryakotharu@users.noreply.github.com>
2020-02-01 12:43:42 +0530

    tempstring
    template & formatted

--- src/index.js
+++ src/index.js
@@ -1,24 +1,3 @@
-/*const http = require("http"); //common js model,es6 supported by v13
-const getQueryString = require("./getQueryString");
-
-//process has methods(of that module) ,properties,functions
-console.log("hi");
-
-http
-  .createServer((request, response) => {
-    //res.writeHead(200,{"Content-Type":"text/html"});
-    response.writeHead(200, { "Content-Type": "application/json" }); //MIME TYPE
-    const queryObject = getQueryString(request.url);
-    response.write(JSON.stringify(queryObject));
-    response.end();
-    //res.write("hello..");
-  })
-  //console.log("wait");
-  .listen(8080);
-//server will listen to request port
-
-//wikipedia.com/books/?isbn=20034324
-*/
 const express = require("express");
 
 const app = express(); //app is instance and has to listen to some port
